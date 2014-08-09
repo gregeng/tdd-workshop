b27b4d1d16ec3a554e019b88b0d4a71b07e4ba56
gregeng <greg.eng@gmail.com>
2014-08-09 13:51:52 -0700

    use let syntax instead of before to get rid of instance variables

--- bowling-game/bowling_game_spec.rb
+++ bowling-game/bowling_game_spec.rb
@@ -2,20 +2,18 @@ require_relative "./bowling_game.rb"
 require 'rspec'
 
 describe BowlingGame do
-  before do
-    @game = BowlingGame.new
-  end
+  let(:game) { BowlingGame.new }
 
   it 'scores gutters as a 0' do
-    20.times { @game.roll(0) }
+    20.times { game.roll(0) }
 
-    expect(@game.score).to eq(0)
+    expect(game.score).to eq(0)
   end
 
   it 'scores all 1s with 20' do
-    20.times { @game.roll(1) }
+    20.times { game.roll(1) }
 
-    expect(@game.score).to eq(20)
+    expect(game.score).to eq(20)
   end
 
 
